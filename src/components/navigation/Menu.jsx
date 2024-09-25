3a385c9958d2d390ab857c880f2fef1fc532cb9d
Daniel Britto <danielbritto185@gmail.com>
2024-09-25 18:01:10 -0500

    final dia 2

--- src/components/navigation/Menu.jsx
+++ src/components/navigation/Menu.jsx
@@ -8,9 +8,9 @@ export default function Menu(){
             <h1 className="font-bold">Volunteer</h1>
             <ul className="flex justify-around w-96">
                 <Link to={"/Home"}>Inicio</Link>
-                <Link to={"/Convocatoria"}>convocatoria</Link>
+                <Link to={"/Convocatoria"}>Convocatoria</Link>
                 <Link to={"/Voluntario"}>Voluntariado</Link>
-                <Link to={"/CrearConvo"}>crear convocatoria</Link>
+                <Link to={"/CrearConvo"}>Crear convocatoria</Link>
             </ul>
             <button className="bg-white border border-gray-300 text-black font-bold py-0 px-4 rounded pt-0"><Link to={"/Login"}>Login</Link></button>
         </div>
